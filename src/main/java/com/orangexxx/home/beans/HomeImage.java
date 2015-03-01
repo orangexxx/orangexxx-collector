188651ae38160ee5045f11bc4c2ee84b85836698
orangexxx <xiayangxxx@gmail.com>
2015-03-01 22:48:27 +0800

    add bean info

---
+++ src/main/java/com/orangexxx/home/beans/HomeImage.java
@@ -0,0 +1,37 @@
+package com.orangexxx.home.beans;
+
+public class HomeImage extends ObjectBean {
+
+	
+	private int mID;
+	private int mHomeID;
+	private String mURL;
+	private String mInfo;
+	public int getmID() {
+		return mID;
+	}
+	public void setmID(int mID) {
+		this.mID = mID;
+	}
+	public int getmHomeID() {
+		return mHomeID;
+	}
+	public void setmHomeID(int mHomeID) {
+		this.mHomeID = mHomeID;
+	}
+	public String getmURL() {
+		return mURL;
+	}
+	public void setmURL(String mURL) {
+		this.mURL = mURL;
+	}
+	public String getmInfo() {
+		return mInfo;
+	}
+	public void setmInfo(String mInfo) {
+		this.mInfo = mInfo;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/orangexxx/home/beans/HomeInfo.java
@@ -0,0 +1,91 @@
+package com.orangexxx.home.beans;
+
+public class HomeInfo extends ObjectBean {
+
+	private int mID;
+	private String mTitle;
+	private String mSpace;
+	private String mStyle;
+	private String mPart;
+	private String mShape;
+	private String mStuff;
+	private String mDesigner;
+	private String mBudget;
+	private String mSize;
+	private String mFeel;
+	private String mColor;
+	public int getmID() {
+		return mID;
+	}
+	public void setmID(int mID) {
+		this.mID = mID;
+	}
+	public String getmTitle() {
+		return mTitle;
+	}
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+	public String getmSpace() {
+		return mSpace;
+	}
+	public void setmSpace(String mSpace) {
+		this.mSpace = mSpace;
+	}
+	public String getmStyle() {
+		return mStyle;
+	}
+	public void setmStyle(String mStyle) {
+		this.mStyle = mStyle;
+	}
+	public String getmPart() {
+		return mPart;
+	}
+	public void setmPart(String mPart) {
+		this.mPart = mPart;
+	}
+	public String getmShape() {
+		return mShape;
+	}
+	public void setmShape(String mShape) {
+		this.mShape = mShape;
+	}
+	public String getmStuff() {
+		return mStuff;
+	}
+	public void setmStuff(String mStuff) {
+		this.mStuff = mStuff;
+	}
+	public String getmDesigner() {
+		return mDesigner;
+	}
+	public void setmDesigner(String mDesigner) {
+		this.mDesigner = mDesigner;
+	}
+	public String getmBudget() {
+		return mBudget;
+	}
+	public void setmBudget(String mBudget) {
+		this.mBudget = mBudget;
+	}
+	public String getmSize() {
+		return mSize;
+	}
+	public void setmSize(String mSize) {
+		this.mSize = mSize;
+	}
+	public String getmFeel() {
+		return mFeel;
+	}
+	public void setmFeel(String mFeel) {
+		this.mFeel = mFeel;
+	}
+	public String getmColor() {
+		return mColor;
+	}
+	public void setmColor(String mColor) {
+		this.mColor = mColor;
+	}
+	
+	
+}
